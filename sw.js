f5db1432d10bba0f16c5d7007996a7ea465cdb8b
Furlaneto1995 <54684464+Furlaneto1995@users.noreply.github.com>
2026-09-16 13:34:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    App shell em cache-first; retratos do CDN em stale-while-revalidate. */
 'use strict';
 
-const VERSION    = 'v6';
+const VERSION    = 'v7';
 const SHELL      = `siege-shell-${VERSION}`;
 const PORTRAITS  = `siege-portraits-${VERSION}`;
 const CDN        = 'https://raw.githubusercontent.com/swarfarm/swarfarm/';
